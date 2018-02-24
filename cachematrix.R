353041aeb607d1642f233b1e62d5ca76ded03def
Anj <anjanishp@gmail.com>
2018-02-24 08:03:26 +0530

    submitting assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file is submission of Assignment 2, where we exploit lexical scoping, 
+## and try to cache the result of matrix inverse, to avoid repeating the 
+## operation.  
 
-## Write a short comment describing this function
+
+## Description: Function responsible to cache a matrix and it's inverse and 
+##              provide API to set or get a matrix and set or
+##              get inverse of a matrix. 
+## Function   : makeCacheMatrix
+## Input      : Matrix
+## Output     : list object of functions operating on Matrix, to set/get matrix 
+##              and setInverse/getInverse inverse of matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseMatrix <- NULL
+    set <- function(y) {
+        x <<- y
+        inverseMatrix <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inverseMatrix <<- inverse
+    getInverse <- function() inverseMatrix
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Description : Function to return inverse of a matrix, either from cache or
+##               by calculating it. 
+## Function    : cacheSolve
+## Input       : list object of function containing set, get, setInverse and 
+##               getInverse for a matrix.
+## Output      : Inverse Matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverseMatrix <- x$getInverse()
+    if(!is.null(inverseMatrix)) {
+        print("we getting inverse matrix from cache")
+        inverseMatrix
+    }
+    matrix <- x$get()
+    inverseMatrix <- solve(matrix)
+    x$setInverse(inverseMatrix)
+    inverseMatrix
 }
